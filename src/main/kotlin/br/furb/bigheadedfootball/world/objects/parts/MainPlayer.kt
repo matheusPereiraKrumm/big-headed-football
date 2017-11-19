fde31a26e179bb9e5a112531b075dcd79e4ae3f3
matheus.periera <sodkrumm@gmail.com>
2017-11-19 13:46:33 -0200

    Movimentação mais Fluída

--- src/main/kotlin/br/furb/bigheadedfootball/world/objects/parts/MainPlayer.kt
+++ src/main/kotlin/br/furb/bigheadedfootball/world/objects/parts/MainPlayer.kt
@@ -4,18 +4,48 @@ import br.furb.bigheadedfootball.common.glDrawable
 import br.furb.bigheadedfootball.world.components.Color
 import br.furb.bigheadedfootball.world.components.Point
 import br.furb.bigheadedfootball.world.components.PointCommom
+import com.intellij.util.io.sleep
 import java.awt.event.KeyEvent
 import java.awt.event.KeyListener
+import kotlin.concurrent.thread
 
 class MainPlayer : Player(), KeyListener {
     override var color: Color = Color.RED
+    private val pressed = HashSet<Int>()
 
     init {
         transformation.translation(25.0, 0.0, 60.0)
+        var t = thread {
+            while (true){
+
+                if (pressed.isNotEmpty()) {
+                    val orignPoint = transformation.transformPoint(PointCommom.neutralPoint())
+                    val destinedZPoint = transformation.transformPoint(Point(0.0,0.0, -0.6))
+                    val positiveMoveZPoint = destinedZPoint.diff(orignPoint)
+
+                    if (pressed.contains(UP))
+                        transformation.translation(positiveMoveZPoint)
+
+                    if (pressed.contains(DOWN))
+                        transformation.translation(positiveMoveZPoint.inverted())
+
+                    val radians = 0.2
+                    if (pressed.contains(LEFT))
+                        rotation(radians)
+
+                    if (pressed.contains(RIGTH))
+                        rotation(-radians)
+
+
+                    glDrawable {
+                        display()
+                    }
+                }
+                Thread.sleep(70)
+            }
+        }
     }
 
-
-    private val pressed = HashSet<Int>()
 
     override fun keyTyped(p0: KeyEvent) {}
 
@@ -26,29 +56,7 @@ class MainPlayer : Player(), KeyListener {
     override fun keyPressed(e: KeyEvent) {
         println(e.keyCode)
         pressed.add(e.keyCode)
-        if (pressed.isNotEmpty()) {
-            val orignPoint = transformation.transformPoint(PointCommom.neutralPoint())
-            val destinedZPoint = transformation.transformPoint(Point(0.0,0.0, -1.0))
-            val positiveMoveZPoint = destinedZPoint.diff(orignPoint)
 
-            if (pressed.contains(UP))
-                transformation.translation(positiveMoveZPoint)
-
-            if (pressed.contains(DOWN))
-                transformation.translation(positiveMoveZPoint.inverted())
-
-            val radians = 0.5
-            if (pressed.contains(LEFT))
-                rotation(radians)
-
-            if (pressed.contains(RIGTH))
-                rotation(-radians)
-
-
-            glDrawable {
-                display()
-            }
-        }
     }
 
     override fun keyReleased(e: KeyEvent) {
